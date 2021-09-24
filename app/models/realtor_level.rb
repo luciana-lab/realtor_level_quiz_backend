199b0fc3af55abdb8a75c219b68402841d83bdba
Luciana Sugeta <lulusugeta@gmail.com>
2021-09-24 11:17:54 -0400

    Relationship between tables

--- app/models/realtor_level.rb
+++ app/models/realtor_level.rb
@@ -1,2 +1,4 @@
 class RealtorLevel < ApplicationRecord
+    has_many :options
+    has_many :users
 end
---
+++ db/schema.rb
@@ -0,0 +1,59 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_09_24_141251) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "options", force: :cascade do |t|
+    t.string "content"
+    t.bigint "question_id", null: false
+    t.bigint "realtor_level_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_options_on_question_id"
+    t.index ["realtor_level_id"], name: "index_options_on_realtor_level_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "realtor_levels", force: :cascade do |t|
+    t.string "level"
+    t.text "description"
+    t.string "free_content"
+    t.string "products"
+    t.string "promo_code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.boolean "has_account"
+    t.string "result"
+    t.bigint "realtor_level_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["realtor_level_id"], name: "index_users_on_realtor_level_id"
+  end
+
+  add_foreign_key "options", "questions"
+  add_foreign_key "options", "realtor_levels"
+  add_foreign_key "users", "realtor_levels"
+end
