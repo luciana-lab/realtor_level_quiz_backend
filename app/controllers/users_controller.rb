979745e478abe6a3cdcd33747089ddd398a06200
Luciana Sugeta <lulusugeta@gmail.com>
2021-09-26 11:19:42 -0400

    Create users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: {error: "Something went wrong. Please try again."}
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :has_account)
+    end
 end
