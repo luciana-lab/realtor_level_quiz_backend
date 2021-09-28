da6ec23d1ffa9a9c283aacab4c9b2bdcbf604088
Luciana Sugeta <lulusugeta@gmail.com>
2021-09-28 11:37:52 -0400

    Add realtor level method on option

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,4 +1,11 @@
 class Option < ApplicationRecord
   belongs_to :question
   belongs_to :realtor_level, optional: true
+
+  def level
+    if self.realtor_level
+      self.realtor_level.level
+    end
+  end
+  
 end
--- app/serializers/option_serializer.rb
+++ app/serializers/option_serializer.rb
@@ -1,4 +1,5 @@
 class OptionSerializer < ActiveModel::Serializer
-  attributes :id, :content
+  attributes :id, :content, :level
   belongs_to :question
+  belongs_to :realtor_level
 end
