ffe1be9ae5a061b3a3a7be99321303c445e9b957
Luciana Sugeta <lulusugeta@gmail.com>
2021-09-29 13:53:07 -0400

    Add require params user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,6 @@ class UsersController < ApplicationController
 
     private
     def user_params
-        params.permit(:first_name, :last_name, :email, :has_account, :result)
+        params.require(:user).permit(:first_name, :last_name, :email, :has_account, :result)
     end
 end
