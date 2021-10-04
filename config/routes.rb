916ea12fa09b88834bccd112dd484abe6a63c2e9
Luciana Sugeta <lulusugeta@gmail.com>
2021-10-04 15:37:47 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :options
-  resources :realtor_levels
-  resources :questions
+  resources :users, only: [:index, :create, :update]
+  # resources :options
+  resources :realtor_levels, only: :index
+  resources :questions, only: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
