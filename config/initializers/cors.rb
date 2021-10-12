7df1a9553b7ac098514825d75bb95d8f0fcafbf3
Luciana Sugeta <lulusugeta@gmail.com>
2021-10-12 12:11:03 -0400

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    # origins 'https://luciana-lab.github.io/realtor_level_quiz_frontend'
+    # origins '*'
+    origins 'https://luciana-lab.github.io/realtor_level_quiz_frontend'
 
     resource '*',
       headers: :any,
